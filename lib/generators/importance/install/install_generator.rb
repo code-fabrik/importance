5635850754715c720bc9fdd17b974b18d00e6b79
Lukas_Skywalker <git@lukasdiener.ch>
2025-05-16 11:13:42 +0000

    Add translations

--- lib/generators/importance/install/install_generator.rb
+++ lib/generators/importance/install/install_generator.rb
@@ -12,6 +12,41 @@ module Importance
       def add_route
         route 'mount Importance::Engine, at: "/importance"'
       end
+
+      def add_translations
+        locale_files = Dir.glob(Rails.root.join("config", "locales", "*.yml"))
+
+        if locale_files.empty?
+          # If no locale files exist, create at least en.yml
+          create_file "config/locales/en.yml", {
+            en: {
+              importance: {
+                use_column_as: "Use column as",
+                ignore: "Ignore",
+                save: "Save"
+              }
+            }
+          }.to_yaml
+        else
+          locale_files.each do |file|
+            locale_content = YAML.load_file(file)
+            locale_key = locale_content.keys.first
+
+            # Add translations under the locale key
+            if locale_content[locale_key].is_a?(Hash)
+              locale_content[locale_key]["importance"] = {
+                "use_column_as" => "Use column as",
+                "ignore" => "Ignore",
+                "save" => "Save"
+              }
+
+              # Write back to the file
+              File.write(file, locale_content.to_yaml)
+              say_status :insert, "importance translations into #{file}", :green
+            end
+          end
+        end
+      end
     end
   end
 end
